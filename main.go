5baca8a4b89888f8c5c92b025cc585315466218f
moronibr <moroniramos@gmail.com>
2025-04-14 10:15:58 -0300

    Começo

--- main.go
+++ main.go
@@ -27,32 +27,6 @@ func main() {
 
 	http.HandleFunc("/index", app.IndexHandler)
 
-	http.HandleFunc("/conta", app.ContaHandler)
-
-	http.HandleFunc("/cliente", app.ClientePageHandler)
-
-	http.HandleFunc("/add-account", app.AddAccountHandler)
-
-	http.HandleFunc("/add-cliente", app.AddClienteHandler(dbConn))
-
-	http.HandleFunc("/clientes-handler", app.ClientesHandler)
-
-	http.HandleFunc("/contas-handler", app.ContasHandler)
-
-	http.HandleFunc("/api/contas", app.ApiContasHandler(dbConn))
-
-	http.HandleFunc("/contas/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodPut {
-			app.EditAccountHandler(dbConn)(w, r)
-		} else if r.Method == http.MethodDelete {
-			app.DeleteAccountHandler(dbConn)(w, r)
-		}
-	})
-
-	http.HandleFunc("/api/clientes", app.ApiClientesHandler(dbConn))
-
-	http.HandleFunc("/api/clientes/", app.GetClientByIDHandler(dbConn))
-
 	fmt.Println("Servidor rodando em http://localhost:8000")
 	http.ListenAndServe(":8000", nil)
 }
